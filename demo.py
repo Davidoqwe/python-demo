214250c41263dd79fed7eee8f59bf0d1782c91dc
Davidoqwe <davidhugu2007@gmail.com>
2025-07-13 13:36:59 +0300

    third commit

--- demo.py
+++
@@ -1,2 +0,0 @@
-for x in "Investment":
-    print(x)
---
+++ main.py
@@ -0,0 +1,31 @@
+from encodings import cp037
+
+
+for x in "Investment":
+    print(x)
+
+def my_function(fname, lname):
+    print(fname +" " + lname)
+my_function("Emil", "Refsnes")
+
+x=len(cars)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
